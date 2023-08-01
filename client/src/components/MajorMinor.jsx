fabd6527cddaa771a158bedf6d7abc05f2076719
uynT03 <donguyen.truong03@gmail.com>
2023-08-01 17:13:32 +0700

    Update nav bar

--- client/src/components/MajorMinor.jsx
+++ client/src/components/MajorMinor.jsx
@@ -1,82 +1,119 @@
 import React, { useState } from "react";
-import { Dropdown } from "react-bootstrap";
+import { Dropdown, Navbar, Nav } from "react-bootstrap";
+import "../App.scss"
 
 export const MajorMinor = () => {
-  // State to keep track of the selected options
-  const [selectedMajor, setSelectedMajor] = useState('');
-  const [selectedMinor, setSelectedMinor] = useState('');
+    // State to keep track of the selected options
+    const [selectedMajor, setSelectedMajor] = useState('');
+    const [selectedMinor, setSelectedMinor] = useState('');
 
-  // Function to handle option selection for the major dropdown
-  const handleMajorSelect = (eventKey) => {
-    setSelectedMajor(eventKey);
-  };
+    // Function to handle option selection for the major dropdown
+    const handleMajorSelect = (eventKey) => {
+      setSelectedMajor(eventKey);
+    };
 
-  // Function to handle option selection for the minor dropdown
-  const handleMinorSelect = (eventKey) => {
-    setSelectedMinor(eventKey);
-  };
+    // Function to handle option selection for the minor dropdown
+    const handleMinorSelect = (eventKey) => {
+      setSelectedMinor(eventKey);
+    };
 
-  // Function to display the result based on the selected option for the major dropdown
-  const getMajorResult = () => {
-    return <div>Result for {selectedMajor} on the major dropdown</div>;
-  };
+    // const options = {Option1: "Applied Mathematics", Option2: "Art and Media Studies", Option3: "Computer Science", Option4: "Economics", Option5: "Engineering", Option6: "History", Option7: "Intergrated Science", Option8: "Literature", Option9: "Psychology", Option10: "Social Studies", Option11: "Vietnam Studies"}
 
-  // Function to display the result based on the selected option for the minor dropdown
-  const getMinorResult = () => {
-    return <div>Result for {selectedMinor} on the minor dropdown</div>;
-  };
+    const options = ["Applied Mathematics","Art and Media Studies", "Computer Science", "Economics","Engineering", "History", "Intergrated Science", "Literature","Psychology", "Social Studies", "Vietnam Studies"]
 
-  const options = ["Applied Mathematics", "Art and Media Studies", "Computer Science", "Economics", "Engineering", "History", "Intergrated Science", "Literature", "Psychology", "Social Studies", "Vietnam Studies"]
+    // Function to display the result based on the selected option for the major dropdown
+    const getMajorResult = () => {
+      return options[selectedMajor] || null;
+    };
 
-  return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-md-6">
-          <Dropdown onSelect={handleMajorSelect}>
-            <Dropdown.Toggle variant="primary" id="major-dropdown-basic">
-              Select one option for major
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-                {options.map((option, index) => (
-                    <Dropdown.Item key={index} eventKey={option}>
-                        {option}
-                    </Dropdown.Item>
-                ))}
-            </Dropdown.Menu>
-          </Dropdown>
-          <div className="mt-3">
-            {selectedMajor ? (
-              getMajorResult()
-            ) : (
-              <div className="text-muted">Select an option for major to see the result.</div>
-            )}
-          </div>
+    // Function to display the result based on the selected option for the minor dropdown
+    const getMinorResult = () => {
+      return options[selectedMinor] || null;
+    };
+
+    const titleStyle = {
+      textAlign: 'center',
+      fontWeight: 'bold',
+      marginBottom: '18px',
+      color: 'var(--bs-secondary)',
+    };
+
+    const linkStyle = {
+      color:'white',
+      fontWeight:'500',
+    }
+
+    return (
+      <div className="container">
+        <div className="navbar fixed-top navbar-expand-lg navbar-light bg-black">
+          <p className="webName text-center" style={{
+            position:'relative',
+            fontSize:22, 
+            color:"white", 
+            marginLeft: '45px',
+            marginTop: '25px'}}>ONECOMPASS</p>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto" style = {{marginRight: "40px"}}>
+                <Nav.Link href="#" style = {linkStyle}>Major/Minor</Nav.Link>
+                <Nav.Link href="#" style = {linkStyle}>Exploratory</Nav.Link>
+                <Nav.Link href="#" style = {linkStyle}>Upcoming Term</Nav.Link>
+                <Nav.Link href="#" style = {linkStyle}>Roadmap</Nav.Link>
+                <Nav.Link href="#" style = {linkStyle}>GPA & Credits</Nav.Link>
+                <Nav.Link href="./components/Profile.jsx" style = {linkStyle}>Profile</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
         </div>
+        <div style={{ marginTop: "100px" }}>
+          <div className="d-flex justify-content-center">
+            <div className="col">
+              <div className="dropdown-title" style = {titleStyle}> Majors </div>
+              <Dropdown onSelect={handleMajorSelect}>
+                <Dropdown.Toggle variant="primary" id="major-dropdown-basic" style = {{width:'500px'}}>
+                  Select one option for major
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  {options.map((option, index) => (
+                      <Dropdown.Item key={index} eventKey={option}>
+                          {option}
+                      </Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              </Dropdown>
+              <div className="mt-3">
+                {selectedMajor ? (
+                  getMajorResult()
+                ) : (
+                  <div className="text-muted" style={{fontSize: '50px',position:'relative'}}>PLEASE CHOOSE YOUR MAJOR!?</div>
+                )}
+              </div>
+            </div>
 
-        <div className="col-md-6">
-          <Dropdown onSelect={handleMinorSelect}>
-            <Dropdown.Toggle variant="primary" id="minor-dropdown-basic">
-                Select one option for major or minor
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-                {options.map((option, index) => (
-                    <Dropdown.Item key={index} eventKey={option}>
-                        {option}
-                    </Dropdown.Item>
-                ))}
-            </Dropdown.Menu>
-          </Dropdown>
-          <div className="mt-3">
-            {selectedMinor ? (
-              getMinorResult()
-            ) : (
-              <div className="text-muted">Select an option for the minor dropdown to see the result.</div>
-            )}
+            <div className="col">
+              <div className="dropdown-title" style = {titleStyle}> Majors/Minor </div>
+              <Dropdown onSelect={handleMinorSelect}>
+                <Dropdown.Toggle variant="primary" id="minor-dropdown-basic" style = {{width:'500px'}}>
+                    Select one option for major or minor
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  {options.map((option, index) => (
+                      <Dropdown.Item key={index} eventKey={option}>
+                          {option}
+                      </Dropdown.Item>
+                  ))}
+                </Dropdown.Menu>
+              </Dropdown>
+              <div className="mt-3">
+                {selectedMinor ? (
+                  getMinorResult()
+                ) : (
+                  <div className="text-muted" style={{fontSize: '50px',position:'relative'}}>PLEASE CHOOSE YOUR MAJOR?!</div>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default MajorMinor;
