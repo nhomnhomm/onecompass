67637e13383e22a4735cacef6b32f123e9d48f38
uynT03 <donguyen.truong03@gmail.com>
2023-08-02 21:21:19 +0700

    Update About us

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const AboutUs = () => {
 
     return (
-        <div> </div>
+        <div>
+            <h5> lỡ có không đẹp thì: </h5>
+            <div className="d-flex justify-content-center align-middle">
+                <iframe width="800" height="500" src="https://www.youtube.com/embed/D3BCfZqNasM" title="iu Anh Phan" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+        </div>
     );
 };
 
